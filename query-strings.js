9684d23c963bdda20340a645a2a0dc907119f68a
BrivaHamisi <brivahamisi@gmail.com>
2025-09-15 16:48:47 +0300

    Query Strings

--- query-strings.js
+++ query-strings.js
@@ -1,6 +1,6 @@
 const http = require('http');
-const url = require('url');
-const queryString = require('querystring');
+const { URL } = require('url');
+const querystring = require('querystring');
 
 http.createServer((req, res) => {
     const baseURL = `http://${req.headers.host}/`; // Construct the base URL from the request headers
@@ -10,11 +10,11 @@ http.createServer((req, res) => {
 
     const queryParams = { ...params, rating: 8.7 }; // Add a new query parameter 'rating'
 
-    const queryString = queryString.stringify(queryParams); // Convert the updated query parameters back to a query string
+    const queryString = querystring.stringify(queryParams); // Convert the updated query parameters back to a query string
 
     res.writeHead(200, { 'Content-Type': 'application/json' });
     res.end(JSON.stringify({ baseURL, pathname: parsedUrl.pathname, queryString })); // Respond with the pathname and updated query string as JSON
 }).listen(3000);
 
 console.log('Server running at http://localhost:3000/'); // This server responds with the updated query string including the new 'rating' parameter.
-// This server responds with "Hello World" and displays the request method and URL.const queryString = require('querystring');          
+// This server responds with "Hello World" and displays the request method and URL.const queryString = require('querystring');  
